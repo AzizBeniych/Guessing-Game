32f2f3cda0ba9f6937a03102900086d143ddd7e2
AzizBeniych <92027564+AzizBeniych@users.noreply.github.com>
2021-12-03 10:10:12 +0100

    FINISH

---
+++ app.js
@@ -0,0 +1,12 @@
+// const btn= document.getElementById("btn");
+
+// btn.addEventListener('click', function(){
+//   var name = document.getElementById("goinput").value;
+//   alert("Name: "+ name);
+// });
+// var num = window.prompt("Enter Number");
+// console.log( "hello",number )
+// document.getElementById('btn').onclick = Function(){
+    // var yournum = document.getElementById('goinput').value;
+    // console.log('hello man' ,yournum);
+// }
